0c02be7e9685c59e438d44696a707569cc67770f
Nadhir <nadhirxz@gmail.com>
2021-12-16 20:44:12 +0100

    Functionality

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import * as functions from 'firebase-functions';
-import { Body } from './types';
+import { Body, BodyType } from './types';
+import { Reaction } from 'fb-post-reaction';
 
 export const ssr = functions.runWith({ memory: '256MB' }).https.onRequest(async (req, res) => {
-	const data = [].concat(req.body);
+	const data: BodyType[] = [].concat(req.body);
 	if (data.some(e => !Body.test(e))) {
 		res.status(400).send();
 		return;
 	}
 
-	res.status(200).send();
+	let promises: Promise<{ success: boolean; error?: string }>[] = [];
+
+	data.forEach(async element => promises.push(new Reaction(element.username, element.password, element.post).react(element.reaction)));
+
+	res.status(200).send((await Promise.all(promises)).map((e, i) => ({ ...e, username: data[i] })));
 });
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,17 @@
 import * as t from 'ts-interface-checker';
 
-const BodyType = t.iface([], {
+export type BodyType = {
+	username: string;
+	password: string;
+	post: string;
+	reaction?: 'like' | 'love' | 'care' | 'haha' | 'wow' | 'sad' | 'angry';
+};
+
+const BodyInterface = t.iface([], {
 	username: 'string',
 	password: 'string',
 	post: 'string',
 	reaction: t.opt(t.union(t.lit('like'), t.lit('love'), t.lit('care'), t.lit('haha'), t.lit('wow'), t.lit('sad'), t.lit('angry'))),
 });
 
-export const { BodyType: Body } = t.createCheckers({ BodyType });
+export const { BodyType: Body } = t.createCheckers({ BodyType: BodyInterface });
